9b0a11fd26132660d1d80735d5bfcfb953c7b3c7
Thomas Brockington <xtombrock1989@gmail.com>
2023-01-23 22:38:46 +0000

    need to fix mui to continue

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,24 @@
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Box } from '@mui/material'
+import Navbar from './components/navbar/Navbar'
+import Feed from './pages/feed/Feed'
+import VideoDetail from './pages/video/VideoDetail'
+import ChannelDetail from './pages/channel/ChannelDetail'
+import SearchFeed from './pages/search/SearchFeed'
 
 function App() {
   return (
     <>
-    hello
+      <Box sx={{ backgroundColor: '#000'}}>
+        
+        <Navbar />
+        <Routes>
+          <Route path='/' exact element={<Feed />} />
+          <Route path='/video/:id' element={<VideoDetail />} />
+          <Route path='/channel/:id' element={<ChannelDetail />} />
+          <Route path='/search/:searchTerm' element={<SearchFeed />} />
+        </Routes>
+      </Box>
     </>
   );
 }
